eaa436444e34d718e9b5746f02375d88ce6be044
panzhihui <panzhihui@xiaomi.com>
2026-05-09 14:06:24 +0800

    Rename to ghost-cd-to, reuse Ghostty instance via AppleScript
    - Rename app to ghost-cd-to
    - When Ghostty is already running: activate it, open new tab (Cmd+T),
      paste cd command from clipboard, then clear screen to hide the command
    - When Ghostty is not running: use open -na for clean first launch
    - Remove ASSETCATALOG_COMPILER_APPICON_NAME to fix Ghostty icon display

--- cd to .../cd to .../main.m
+++ cd to .../cd to .../main.m
@@ -8,6 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 #import <ScriptingBridge/ScriptingBridge.h>
+#import <unistd.h>
 
 #import "Finder.h"
 
@@ -38,12 +39,55 @@ int main(int argc, const char * argv[]) {
         NSURL* url = [NSURL URLWithString:fileUrl];
         if (url != nil){
             NSString *dirPath = [url path];
+            NSString *arg = [@"--working-directory=" stringByAppendingString:dirPath];
 
-            // Launch Ghostty with --working-directory via open -na
-            NSTask *task = [[NSTask alloc] init];
-            [task setLaunchPath:@"/usr/bin/open"];
-            [task setArguments:@[@"-na", @"Ghostty.app", @"--args", [@"--working-directory=" stringByAppendingString:dirPath]]];
-            [task launch];
+            // Check if Ghostty is already running
+            NSArray *running = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.mitchellh.ghostty"];
+
+            if ([running count] == 0) {
+                // First launch: use open -na for a clean start (no visible cd command)
+                NSTask *task = [[NSTask alloc] init];
+                [task setLaunchPath:@"/usr/bin/open"];
+                [task setArguments:@[@"-na", @"Ghostty.app", @"--args",
+                                     [@"--working-directory=" stringByAppendingString:dirPath]]];
+                [task launch];
+            } else {
+                // Already running: open new tab via AppleScript, paste cd command, then clear screen
+                // Requires accessibility permissions for System Events.
+
+                // Save clipboard
+                NSPasteboard *pb = [NSPasteboard generalPasteboard];
+                NSString *oldString = [pb stringForType:NSPasteboardTypeString];
+
+                // Build command: cd + clear screen (hide the cd command from view)
+                NSString *cdCommand = [NSString stringWithFormat:@"cd '%@' && clear", dirPath];
+                [pb clearContents];
+                [pb setString:cdCommand forType:NSPasteboardTypeString];
+
+                NSString *script =
+                    @"tell application \"Ghostty\" to activate\n"
+                     "delay 0.2\n"
+                     "tell application \"System Events\" to keystroke \"t\" using command down\n"
+                     "delay 0.5\n"
+                     "tell application \"System Events\" to keystroke \"v\" using command down\n"
+                     "delay 0.1\n"
+                     "tell application \"System Events\" to key code 36";
+                NSDictionary *error = nil;
+                NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:script];
+                [appleScript executeAndReturnError:&error];
+                if (error) {
+                    NSLog(@"AppleScript error: %@", error);
+                }
+
+                // Restore clipboard
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    NSPasteboard *pb = [NSPasteboard generalPasteboard];
+                    [pb clearContents];
+                    if (oldString) {
+                        [pb setString:oldString forType:NSPasteboardTypeString];
+                    }
+                });
+            }
         }
     }
 }
